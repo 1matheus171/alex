7eece6b774a30d213d5621ea79e397ddae2921b9
1matheus171 <matheus.mix211@gmail.com>
2024-10-17 11:58:53 -0300

    Add files via upload

---
+++ goti/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'matematica',
+    '1+1',
+    '2'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'cassino',
+    'jogo pra forrar',
+    'tigrinho'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
